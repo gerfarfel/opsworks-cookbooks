00b1bf6b0af93109301a169fa09ddb217cf51b9e
Dick Fryling <dick@doink.com>
2013-10-31 20:43:02 -0400

    Added java and deploy cookbooks.

---
+++ deploy-propmaster-0.0.1/files/default/backup-db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+bucket=doink-backup
+
+function backup() {
+ what=$1
+
+ database=$what
+ temp_dir=$(mktemp -d /tmp/$what.XXXXXXX)
+ trap "rm -rf $temp_dir" INT EXIT
+ cd $temp_dir
+
+ folder=mongoBackup
+ when=$(date '+%Y-%m-%d-%H-%M-%S')
+ backup_file=$what-$when.tar.gz
+ mongodump -d $database -o $folder
+ tar --create --gzip --file $backup_file $folder
+ s3cmd  -c /home/ubuntu/.s3cfg put $backup_file s3://$bucket/$database/$backup_file
+}
+
+backup propmaster
