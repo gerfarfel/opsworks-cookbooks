fe003ab4a105b068e2603eda3f0805c4a2509248
Dick Fryling <dick@doink.com>
2013-11-01 09:05:20 -0400

    Added mongodb cookbook. Modified s3cmd cookbook.

--- s3cmd/recipes/default.rb
+++ s3cmd/recipes/default.rb
@@ -8,8 +8,10 @@ package "s3cmd" do
   action :upgrade
 end
 
+home = user == :root ? "/root" : "/home/ubuntu"
+
 template "s3cfg" do
-  path "/home/ubuntu/.s3cfg"
+  path "#{home}/.s3cfg"
   source "s3cfg.erb"
   mode 0655
   variables({
