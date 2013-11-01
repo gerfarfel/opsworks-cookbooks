480629a0b7b88e9a2f64f049fc28c87ce40645b2
Dick Fryling <dick@doink.com>
2013-11-01 09:42:52 -0400

    Modified s3cmd cookbook.

--- s3cmd/recipes/default.rb
+++ s3cmd/recipes/default.rb
@@ -8,14 +8,17 @@ package "s3cmd" do
   action :upgrade
 end
 
-home = user == :root ? "/root" : "/home/ubuntu"
-
-template "s3cfg" do
-  path "#{home}/.s3cfg"
-  source "s3cfg.erb"
-  mode 0655
-  variables({
-    :aws_access_key_id => node[:aws_access_key_id],
-    :aws_secret_access_key => node[:aws_secret_access_key]
-  })
-end  
+node[:s3cmd][:users].each do |user| 
+   
+  home = user == :root ? "/root" : "/home/#{user}"
+   
+  template "s3cfg" do
+    path "#{home}/.s3cfg"
+    source "s3cfg.erb"
+    mode 0655
+    variables({
+      :aws_access_key_id => node[:aws_access_key_id],
+      :aws_secret_access_key => node[:aws_secret_access_key]
+    })
+  end 
+end
