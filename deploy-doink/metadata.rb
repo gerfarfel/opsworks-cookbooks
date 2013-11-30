9f992730396c6086128013c96a0b5aaf43071ca7
Dick Fryling <dick@doink.com>
2013-11-30 09:57:05 -0500

    Added a deploy cookbook for the doink-unfiltered server.

---
+++ deploy-doink/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "DK Pictures, Inc."
+maintainer_email "dick@doink.com"
+license          "All rights reserved"
+description      "Installs/Configures deploy-propmaster"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ deploy-doink/recipes/default.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: deploy-propmaster
+# Recipe:: default
+#
+# Copyright 2013, DK PIctures, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Transfer jar files from S3 to the server.
+execute "s3cmd" do
+  command "mkdir /home/ubuntu/doink-unfiltered ; s3cmd sync --recursive s3://doink-unfiltered/target /home/ubuntu/doink-unfiltered/"
+  cwd "/home/ubuntu"
+  action :run
+end
+
+# Fire up the server as a service!
+runit_service "deploy-doink-unfiltered"
+
+# Install the database backup script.
+cookbook_file "/usr/bin/backup-db.sh" do
+  source "backup-db.sh"
+  mode 0755
+  owner "root"
+  group "root"
+end
+
+# Set up cron tasks to perform backups at scheduled intervals.
+# cron "backup-db_0300" do
+#   hour "3"
+#   minute "0"
+#   command "/usr/bin/backup-db.sh"
+# end
+
+# cron "backup-db_0900" do
+#   hour "9"
+#   minute "0"
+#   command "/usr/bin/backup-db.sh"
+# end
+
+# cron "backup-db_1500" do
+#   hour "15"
+#   minute "0"
+#   command "/usr/bin/backup-db.sh"
+# end
+
+# cron "backup-db_2100" do
+#   hour "21"
+#   minute "0"
+#   command "/usr/bin/backup-db.sh"
+# end
